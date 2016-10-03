9fd53c09e4c8eb208fdf5d979a78c3652802e0ad
josh schank <jps364@drexel.edu>
2016-10-03 16:01:35 -0400

    Schank- Item base class and updated Player class

---
+++ Assets/Scripts/Player/Item.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+    private int quantity=0;
+    protected string flavorText;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void AddItem()
+    {
+        quantity++;
+    }
+    public string ViewText()
+    {
+        return flavorText;
+    }
+    public void Effect() { }
+}
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Player : MonoBehaviour {
     protected string horizontal = "Horizontal"; 
     protected string vertical = "Vertical";
+    private Transform myTransform;
     private float movementSpeed = 5f;  
     private float moveHorizontal;
     private float moveVertical;
@@ -11,11 +12,11 @@ public class Player : MonoBehaviour {
 
     // Use this for initialization
     void Start() {
-
+        myTransform = GetComponent<Transform>();
     }
     // Update is called once per frame
     void Update() {
-
+        Movement(myTransform);
     }
     public void Movement(Transform transform)
     {
